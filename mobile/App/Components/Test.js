ef7d7d919e08f9327f4f46d012f42e60ddabdc7a
VDUCHEW <vidiuchiu@gmail.com>
2015-12-04 10:58:30 -0800

    [feature]mobile navbar

---
+++ mobile/App/Components/Test.js
@@ -0,0 +1,49 @@
+const React = require('react-native');
+
+const {
+	View,
+	Dimensions,
+	Image,
+	Text,
+	StyleSheet
+} = React;
+
+const {width, height} = Dimensions.get('window');
+
+class Test extends React.Component {
+	render() {
+		return (
+			<View style={styles.container}>
+				<View style={styles.bgImageWrapper}>
+					<Image
+						style={styles.bgImage}
+						source={require('../Assets/splash-screen.jpg')}/>
+				</View>
+				<Text>
+					This is some test with React-Native
+				</Text>
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#96FFAB',
+		flexDirection: 'column',
+		height: 100,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	bgImageWrapper: {
+		position: 'absolute',
+		bottom: 0, left: 0
+	},
+	bgImage: {
+		flex: 1,
+		width, height
+	}
+});
+
+module.exports = Test;
