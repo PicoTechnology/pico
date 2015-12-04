d074b9594cdf10bb16e46f3e0479b1a3b0440f7c
William <wpcarro@wustl.edu>
2015-12-03 17:33:05 -0800

    .gitignore is now working

--- server/credentials.js
+++
@@ -1,4 +0,0 @@
-const client_id = '4a9dd390455f4271b2addb5f798bda94';
-const client_secret = '2948fc30f5574060bc13c24b0d052dcd'; // do not publish
-
-module.exports = Object.assign({}, {client_id, client_secret});
