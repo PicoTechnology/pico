5294a5aeeaf68533a3c471a80966552bba973060
William <wpcarro@wustl.edu>
2015-12-04 15:37:25 -0800

    Adds hashing algorithm for device fingerprinting

--- server/server-helpers.js
+++ server/server-helpers.js
@@ -3,6 +3,31 @@ const printRequestInfo = (req, res, next) => {
 	next();
 };
 
+const printUserIP = (req, res, next) => {
+	console.log(`IP Address: ${retrieveUserIP(req)}`);
+	next();
+};
+
+const retrieveUserIP = req => req.connection.remoteAddress;
+
+const printRequestHeaders = (req, res, next) => {
+	console.log(`header: ${req.headers['user-agent']}`);
+	console.log(`device hash: ${hashUserAgent(req.headers['user-agent'])}`);
+	next();
+};
+
+const hashUserAgent = userAgentString => {
+	// remove whitespace from string
+	userAgentString = userAgentString.replace(/\s+/g, '');
+	var h = 0;
+	for (var i=0; i<userAgentString.length; i++) {
+		h = 131 * h + userAgentString.charAt(i).charCodeAt();
+	}
+	return h;
+};
+
 module.exports = {
-	printRequestInfo
+	printRequestInfo,
+	printUserIP,
+	printRequestHeaders
 };
