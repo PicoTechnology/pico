7da7312935e210975f3d09eee727ea10dce5857c
VDUCHEW <vidiuchiu@gmail.com>
2015-12-04 19:28:50 -0800

    [feature]styling/navbar

--- browser/app/components/Navbar.js
+++ browser/app/components/Navbar.js
@@ -4,50 +4,30 @@ var React = require('react');
 var Navbar = React.createClass({
   render: function() {
     return (
-      <div className="navbarStyle" style={styles.navbarStyle}>
-
-      <ul id="navbar">
-      	<li><a href="#">Home</a></li>
-      	<li><a href="#">About</a></li>
-      	<li><a href="#">Portfolio</a></li>
-      	<li><a href="#">Blog</a></li>
-      	<li><a href="#">Contact</a></li>
-      </ul>
-
-
         <div className="container-fluid">
-        <nav className="navbar navbar-fixed-top">
-        <div className="navbar-header">
-          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-            <span className="sr-only">Toggle navigation</span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-          </button>
-          <a className="navbar-brand" href="#">PICO TECHNOLOGY(LOGO)</a>
-          </div>
-            <div className="container-fluid">
-              <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul className="nav navbar-nav navbar-right">
-                  <li><a href="#home">What's PICO?</a></li>
-                  <li><a href="#team">Team</a></li>
-                </ul>
-              </div>
+        <nav className="navbar navbar-inverse navbar-fixed-top">
+          <div className="container-fluid" className="the-blur">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <a className="navbar-brand" href="#">PICO TECHNOLOGY(LOGO)</a>
             </div>
-          </nav>
-  	    </div>
-      </div>
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul className="nav navbar-nav navbar-right">
+                <li><a href="#home">What's PICO?</a></li>
+                <li><a href="#team">Team</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+	    </div>
 		);
   }
 });
-
-
-var styles = {
-  navbarStyle: {
-    backgroundColor: 'red'
-  },
-
-};
 
 
 
