d453b0a796bb7e4a3c909cd48ce3fc99986bfa1a
VDUCHEW <vidiuchiu@gmail.com>
2015-12-04 19:28:28 -0800

    [feature]styling/navbar

--- mobile/App/Components/Test.js
+++ mobile/App/Components/Test.js
@@ -1,56 +1,55 @@
 const React = require('react-native');
 
-const {
-	View,
-	Dimensions,
-	Image,
-	Text,
-	StyleSheet,
-	TouchableHighlight
+var {
+  StyleSheet,
+  View,
+  TouchableHighlight,
+  Text
 } = React;
 
-const {width, height} = Dimensions.get('window');
-
-
-
-
-
-
-
 class Test extends React.Component {
-	render() {
-		return (
-			<View style={styles.container}>
-				<View style={styles.bgImageWrapper}>
-					<Image
-						style={styles.bgImage}
-						source={require('../Assets/splash-screen.jpg')}/>
-				</View>
-				<Text>
-					This is some test with React-Native AHHH
-				</Text>
-			</View>
-		);
-	}
-}
+  goDerper() {
+    this.props.navigator.push({
+               title: 'The child title',
+               component: Child,
+               passProps: {myElement: 'this could be your value!'}
+           });
+  },
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableHighlight
+          style={styles.button}
+          onPress={() => this.goDerper()}
+          underlayColor='#bbbbbb'>
+          <Text style={styles.btnText}>We must go derper</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+})
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#96FFAB',
-		flexDirection: 'column',
-		height: 100,
-		justifyContent: 'center',
-		alignItems: 'center'
-	},
-	bgImageWrapper: {
-		position: 'absolute',
-		bottom: 0, left: 0
-	},
-	bgImage: {
-		flex: 1,
-		width, height
-	}
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#5151f4',
+    padding: 10
+  },
+  button: {
+    height: 36,
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#123456',
+    justifyContent: 'center',
+    color: '#FFFFFF',
+  },
+  btnText: {
+    fontSize: 18,
+    color: '#fff',
+    marginTop: 6,
+  }
 });
 
 module.exports = Test;
