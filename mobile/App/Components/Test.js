f98ba6009896c53bc2ae28fabbc950be5d5ca887
VDUCHEW <vidiuchiu@gmail.com>
2015-12-07 15:31:15 -0800

    [fix]update to the latest code

--- mobile/App/Components/Test.js
+++ mobile/App/Components/Test.js
@@ -1,55 +1,74 @@
 const React = require('react-native');
 
 var {
-  StyleSheet,
+  Text,
   View,
+  NavigatorIOS,
+  Image,
+  StyleSheet,
   TouchableHighlight,
-  Text
+  Dimensions
 } = React;
 
+const {width, height} = Dimensions.get('window');
+
 class Test extends React.Component {
-  goDerper() {
-    this.props.navigator.push({
-               title: 'The child title',
-               component: Child,
-               passProps: {myElement: 'this could be your value!'}
-           });
-  },
-  render() {
-    return (
-      <View style={styles.container}>
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.bgImageWrapper}>
+                    <Image
+                        style={styles.bgImage}
+                        source={require('../Assets/splash-screen.jpg')}/>
+                </View>
         <TouchableHighlight
           style={styles.button}
-          onPress={() => this.goDerper()}
-          underlayColor='#bbbbbb'>
-          <Text style={styles.btnText}>We must go derper</Text>
+          underlayColor="green">
+            <Text style={styles.buttonText}>Initial button</Text>
         </TouchableHighlight>
-      </View>
-    );
-  }
-})
+            </View>
+        );
+    }
+}
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: '#5151f4',
-    padding: 10
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#96FFAB',
+        flexDirection: 'column',
+        height: 100,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    bgImageWrapper: {
+        position: 'absolute',
+        bottom: 0, left: 0
+    },
+    bgImage: {
+        flex: 1,
+        width, height
+    },
+  buttonText: {
+    fontSize: 18,
+    color: '#111',
+    alignSelf: 'center'
   },
   button: {
-    height: 36,
-    flex: 1,
+    height: 45,
+    width: 125,
     flexDirection: 'row',
-    backgroundColor: '#123456',
-    justifyContent: 'center',
-    color: '#FFFFFF',
-  },
-  btnText: {
-    fontSize: 18,
-    color: '#fff',
-    marginTop: 6,
+    backgroundColor: 'white',
+    borderColor: 'white',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    marginTop: 10,
+    marginLeft: 10,
+    marginRight: 10,
+    alignSelf: 'auto',
+    justifyContent: 'center'
   }
 });
+
 
 module.exports = Test;
