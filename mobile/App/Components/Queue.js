53c9c942f4698b0cff245c211eb5329b384c46c2
Casandra Silva <silvacasandra@gmail.com>
2015-12-10 16:55:10 -0800

    creates a basic instance of tracks queue

--- mobile/App/Components/Queue.js
+++ mobile/App/Components/Queue.js
@@ -1,24 +1,25 @@
-Queue() {
-	this.storage = [];
-	this.front = 0;
-	this.back = 0;
-};
-
-Queue.prototype.enqueue = function(songId) {
-	this.storage[this.back] = songId;
-	this.back++;
-};
-
-Queue.prototype.dequeue = function() {
-	if (this.size() > 0) {
-		var leaving = this.storage[this.front];
-		delete this.storage[this.front];
-		this.front++;
-		return leaving;
+class Queue {
+	constructor() {
+		this.storage = [];
+		this.front = 0;
+		this.back = 0;
 	}
-	return null;
+	enqueue(songId) {
+		this.storage[this.back] = songId;
+		this.back++;
+	}
+	dequeue() {
+		if (this.size() > 0) {
+			var leaving = this.storage[this.front];
+			delete this.storage[this.front];
+			this.front++;
+			return leaving;
+		}
+		return null;
+	}
+	size() {
+		return this.back - this.front;
+	}
 };
 
-Queue.prototype.size = function() {
-		return this.back - this.front;
-};
+module.exports = Queue;
