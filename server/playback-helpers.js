673462b8c2dea8f7d3c16eddecfd6785ba1d1692
William <wpcarro@wustl.edu>
2015-12-29 11:07:38 -0800

    Adds helper functions for playback

---
+++ server/playback-helpers.js
@@ -0,0 +1,17 @@
+const exec = require('child_process').exec;
+const path = require('path');
+
+module.exports = {
+	playsong: function(songpath) {
+		console.log(`Now playing ${path.basename(songpath)}...`)
+		var ext = path.extname(songpath);
+		switch (ext) {
+			case '.wav':
+				exec(`aplay ${songpath}`);
+				break;
+			default:
+				exec(`mpg123 ${songpath}`);
+				break;
+		}
+	}
+};
