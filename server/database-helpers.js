afbdd100eb3772e248fad4437934b07b1c32669a
William <wpcarro@wustl.edu>
2015-12-29 15:04:59 -0800

    Rephrases console.logs and refactors UI

--- server/database-helpers.js
+++ server/database-helpers.js
@@ -87,7 +87,7 @@ const addToPlaylist = (req, res, next) => {
 };
 
 const addToPartyPlaylist = (req, res, next) => {
-	console.log(`req.body.trackObj: ${JSON.stringify(req.body.trackObj, null, 2)}`);
+	console.log(`Adding "${req.body.trackObj.title}" to the Party Playlist...`);
 	var trackObj = Object.assign({rating: 0}, {soundcloud: req.body.trackObj});
 	PartyPlaylistRef
 		.child(trackObj.soundcloud.id)
@@ -173,7 +173,6 @@ const getPartyPlaylist = (req, res, next) => {
 				exportArr.push(snapshot.exportVal());
 			});
 			res.data = exportArr;
-			console.log(`exportArr: ${JSON.stringify(exportArr, null, 2)}`);
 			next();
 		});
 };
