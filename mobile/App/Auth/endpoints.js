5920162e8c61ce4e8b23417290d0faf88a99de15
William <wpcarro@wustl.edu>
2015-12-29 21:24:51 -0800

    Swaps mplayer in for music player; streams music

--- mobile/App/Auth/endpoints.js
+++ mobile/App/Auth/endpoints.js
@@ -1,3 +1,3 @@
 module.exports = {
-	serverEndpoint: 'http://localhost:8000'
+	serverEndpoint: 'http://192.168.1.41:8000'
 };
--- server/server.js
+++ server/server.js
@@ -28,9 +28,7 @@ app.use(bodyparser.json());
 const client_id = SoundCloudCred.client_id;
 
 app.get('/connect', (req, res, next) => {
-  console.log('Searching for bluetooth devices...');
-  // bluetoothHelpers.beginSearch();
-
+  console.log('Successfully connected to Pico. Welcome!');
   res.send({numUsers: 3});
 });
 
@@ -42,20 +40,22 @@ app.post('/playsong', (req, res, next) => {
   var trackObj = req.body;
   var uri = trackObj.id;
   var queryString = qs.stringify(Object.assign({}, {client_id}));
-  console.log(`download link: ${SOUNDCLOUD_API}/tracks/${uri}/download?${queryString}`);
+  var downloadLink = `${SOUNDCLOUD_API}/tracks/${uri}/download?${queryString}`
+  console.log(`download link: ${downloadLink}`);
 
-  var songpath = `${__dirname}/assets/${trackObj.id}.${trackObj.original_format}`;
-  var mp3File = fs.createWriteStream(songpath);
-  mp3File.on('finish', () => {
-    console.log(`finished downloading ${trackObj.title}`);
-    playbackHelpers.play(songpath);
-    res.send('finished downloading');
-  });
+  playbackHelpers.streamSong(downloadLink);
+  // var songpath = `${__dirname}/assets/${trackObj.id}.${trackObj.original_format}`;
+  // var mp3File = fs.createWriteStream(songpath);
+  // mp3File.on('finish', () => {
+  //   console.log(`finished downloading ${trackObj.title}`);
+  //   playbackHelpers.play(songpath);
+  //   res.send('finished downloading');
+  // });
 
-  request
-    .get(`${SOUNDCLOUD_API}/tracks/${uri}/download?${queryString}`)
-    .on('error', err => console.error(err))
-    .pipe(mp3File)
+  // request
+  //   .get(`${SOUNDCLOUD_API}/tracks/${uri}/download?${queryString}`)
+  //   .on('error', err => console.error(err))
+  //   .pipe(mp3File)
 });
 
 app.post('/users', dbHelpers.authenticateUser, (req, res, next) => {
@@ -152,6 +152,6 @@ app.get('/authorize', (req, res, next) => {
 app.listen(PORT);
 console.log(`Now listening on localhost:${PORT}...`);
 dbHelpers.connectToDB();
-
+playbackHelpers.initPlayer();
 // bluetooth
 //bluetoothHelpers.initializeBluetooth();
