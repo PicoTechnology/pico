edc2bc09afd0a336d4907437512924347800fedf
VDUCHEW <vidiuchiu@gmail.com>
2015-12-30 17:30:35 -0800

    Removes console.logs in server.js

--- browser/server/server.js
+++ browser/server/server.js
@@ -5,7 +5,6 @@ var path = require('path');
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', function (req, res, next) {
-  console.log('sending index.html...');
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
@@ -14,5 +13,4 @@ console.log(path.join(__dirname, '../public'));
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
-  console.log(`The browser server is listening at http://${host}:${port}`);
 });
