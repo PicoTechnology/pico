d2d0a4a0a7a0358b1c2f751177707e4208bd8c1e
Ubuntu <ubuntu@ip-172-31-63-120.ec2.internal>
2015-12-31 02:25:06 +0000

    Fixes deployment code

--- browser/server/server.js
+++ browser/server/server.js
@@ -4,13 +4,9 @@ var path = require('path');
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('/', function (req, res, next) {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
-});
-
 console.log(path.join(__dirname, '../public'));
 
-var server = app.listen(3000, function () {
+var server = app.listen(8080, function () {
   var host = server.address().address;
   var port = server.address().port;
 });
